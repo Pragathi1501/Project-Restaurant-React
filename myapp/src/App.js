66a13fc23f86cfbeafe5806eac553a78c8152766
PragathiJagadade <Pragathi.Jagadade@cognizant.com>
2023-12-28 18:40:27 +0530

    First Challenge

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resturant from "./Basics/Restaurant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./Hooks/useReducer";
+
+const App = () => {
+ // return <UseReducer />;
+ return <Resturant />;
+};
+
+export default App;
